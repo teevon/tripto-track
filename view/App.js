f170db8f850469a57e7deb78d982eb666642f4e7
teevon <tegovona@gmail.com>
2019-10-04 21:25:15 +0100

    Add files via upload

---
+++ view/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Routes from './components/routing/Routes';
+import Register from './containers/Register/Register';
+import Login from './containers/Login/Login';
+import Alert from './components/Alert/Alert';
+import Dashboard from './containers/Dashboard/Dashboard';
+import ResetPassword from './components/ResetPassword/ResetPassword';
+import Forgot from './components/Forgot/Forgot';
+import CheckEmail from './components/CheckEmail/CheckEmail';
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Alert />
+        <Switch>
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route exact path='/add-expense' component={Dashboard} />
+          <Route exact path='/forgot' component={Forgot} />
+          <Route exact path='/check-email' component={CheckEmail}/>
+        <Route exact path='/reset-password/:header/:payload/:signature' component={ResetPassword} />
+        
+          <Routes />
+        </Switch>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
